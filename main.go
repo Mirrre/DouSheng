1104b76c17691b8631d9fd497c292e42aeba7cf8
for shell <fromShell@paas.com>
2023-08-19 23:11:23 +0800

    implemented User related APIs and models

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+  "app/modules/models"
+  "app/modules/user"
+  "os"
+  "fmt"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// 连接到MySQL数据库
+  User := os.Getenv("MYSQL_USER")
+  Pass := os.Getenv("MYSQL_PASSWORD")
+  Host := os.Getenv("MYSQL_HOST")
+  Port := os.Getenv("MYSQL_PORT")
+	// dsn := "user:rootpassword@tcp(db:3306)/app?charset=utf8mb4&parseTime=True&loc=Local"
+  dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/mysql?charset=utf8mb4&parseTime=True&loc=Local", User, Pass, Host, Port)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+  // TODO: defer close db
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// 迁移数据库模式
+	db.AutoMigrate(&models.User{}, &models.UserProfile{})
+
+	r := gin.Default()
+
+  // 注册中间件将db实例传递给每个处理函数
+  r.Use(func(c *gin.Context) {
+      c.Set("db", db)
+      c.Next()
+  })
+  
+	// 设置路由处理函数
+	r.POST("/douyin/user/register/", user.Register)
+	r.GET("/douyin/user/", user.GetUser)
+  r.POST("/douyin/user/login/", user.Login)
+
+	r.Run(":8080")
 }
---
+++ modules/user/apis.go
@@ -0,0 +1,124 @@
+package user
+
+import (
+  "app/modules/models"
+	"errors"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+  "strconv"
+)
+
+// Register 处理用户注册的API请求
+func Register(c *gin.Context) {
+	var user models.User
+
+  user.Username = c.PostForm("Username")
+  user.Password = c.PostForm("Password")
+  if len(user.Username) < 6 || len(user.Password) < 6 || len(user.Username) > 25 || len(user.Password) > 25 {
+      c.JSON(http.StatusBadRequest, gin.H{
+        "status_code": 1,
+        "status_msg": "Username and Password should be non-empty and between 6 - 25 characters.",
+        "user_id": nil,
+        "username": user.Username,
+      })
+      return
+  }
+
+	// 使用GORM将用户数据存储到数据库中
+	db := c.MustGet("db").(*gorm.DB)
+	if err := db.Create(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+        "status_code": 1,
+        "status_msg": "Failed to register",
+        "user_id": nil,
+      })
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+    "status_code": 0,
+    "status_msg": "Registered!",
+    "user_id": user.ID,
+    // "token": "token_string",
+  })
+}
+
+// GetUser 处理获取单个用户的API请求
+func GetUser(c *gin.Context) {
+    // 从路径中获取用户ID
+    id := c.Query("id")
+
+    // 将ID从字符串转换为uint
+    userID, err := strconv.ParseUint(id, 10, 32)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+        return
+    }
+
+    var user models.User
+    db := c.MustGet("db").(*gorm.DB)
+    if err := db.First(&user, userID).Error; err != nil {
+        if errors.Is(err, gorm.ErrRecordNotFound) {
+            c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+        } else {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+        }
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{
+      "status_code": 0,
+      "status_msg": "OK",
+      "user": user,
+    })
+}
+
+// Login 处理用户登录请求
+func Login(c *gin.Context) {
+    var user models.User
+    var inputUser models.User
+
+    // 从 PostForm 中提取用户名和密码
+    inputUser.Username = c.PostForm("Username")
+    inputUser.Password = c.PostForm("Password")
+
+    // 验证用户名密码非空
+    if inputUser.Username == "" || inputUser.Password == "" {
+        c.JSON(http.StatusUnauthorized, gin.H{
+            "status_code": 1,
+            "status_msg": "Username of Password is missing.",
+        })
+        return
+    }
+
+    // 使用GORM检索用户是否存在
+    db := c.MustGet("db").(*gorm.DB)
+    if err := db.Where("username = ?", inputUser.Username).First(&user).Error; err != nil {
+        c.JSON(http.StatusUnauthorized, gin.H{
+            "status_code": 1,
+            "status_msg": "User not found",
+        })
+        return
+    }
+
+    // 验证密码 (这里验证的是明文密码，实际生产中应该使用哈希后的密码)
+    if user.Password != inputUser.Password {
+        c.JSON(http.StatusUnauthorized, gin.H{
+            "status_code": 1,
+            "status_msg": "Incorrect password",
+        })
+        return
+    }
+
+    // TODO: token
+    // token := "generated_token"
+
+    c.JSON(http.StatusOK, gin.H{
+        "status_code": 0,
+        "status_msg": "Logged in successfully",
+        "user_id": user.ID,
+        // "token": token,
+    })
+}
+
